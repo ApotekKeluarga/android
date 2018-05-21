9d5d7e26d20b818e639d91ff7744614f74ed6201
ARDHAN\Ardhan <febriansyahardhan@gmail.com>
2018-05-21 15:29:17 +0700

    ini ngga, kalo ada yang kurang bisa chat....

---
+++ ardhan/Java/Logout.java
@@ -0,0 +1,20 @@
+package com.example.aprian_pc.akonline;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.aprian_pc.akonline.Fragment.HomeFragment;
+import com.example.aprian_pc.akonline.R;
+
+public class Logout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logout);
+        Intent intent = new Intent(this, Login.class);
+        startActivity(intent);
+
+    }
+}
