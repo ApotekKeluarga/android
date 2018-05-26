d45710f0eff0215a611dd81ea2dd050e5f00ac27
ARDHAN\Ardhan <febriansyahardhan@gmail.com>
2018-05-27 05:43:03 +0700

    ini detail obatnya ngga. Manifest nya juga aku update.

--- ardhan/Java/DetailObat.java
+++ ardhan/Java/DetailObat.java
@@ -1,15 +1,15 @@
-package com.example.aprian_pc.akonline;
+package com.example.aprian_pc.akonline.Pages;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.aprian_pc.akonline.R;
 
-public class KonfirmasiPesanan extends AppCompatActivity {
+public class DetailObat extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_konfirmasi_pesanan);
+        setContentView(R.layout.activity_detail_obat);
     }
 }
